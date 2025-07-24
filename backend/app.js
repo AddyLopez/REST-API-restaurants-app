5280fc0dcb038c9348e4a198048e3fd6be7ea8b6
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-07-23 21:42:11 -0600

    Add comment to clarify the modular file structure of the routes

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,22 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+/* This file sets up a modular organization of routes.
+When the routes in app.use() in the bottommost grouping are hit,
+those requests are directed to and handled in the files required in the constants below,
+which are the second parameters in the app.use() functions.
+
+The home page routes are handled in ./routes/index.js
+The restaurants routes are handled in ./routes/restaurants.js
+The starred restaurants routes are handled in ./routes/starredRestaurants.js
+*/
+
 const indexRouter = require("./routes/index");
 
 const restaurantsModule = require("./routes/restaurants");
 const restaurantsRouter = restaurantsModule.router;
 
-const starredRestaurantsRouter = require("./routes/starredRestaurants")
+const starredRestaurantsRouter = require("./routes/starredRestaurants");
 
 const cors = require("cors");
 

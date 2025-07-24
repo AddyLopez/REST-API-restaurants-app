cda9d4eb485541d13da2a8925d76e8645735bed0
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-07-23 22:02:50 -0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const logger = require("morgan");
 /* This file sets up a modular organization of routes.
 When the routes in app.use() in the bottommost grouping are hit,
 those requests are directed to and handled in the files required in the constants below,
-which are the second parameters in the app.use() functions.
+which are the second parameters in the app.use() functions. The format, simply, is app.use('/route', routeHandler).
 
 The home page routes are handled in ./routes/index.js
 The restaurants routes are handled in ./routes/restaurants.js
